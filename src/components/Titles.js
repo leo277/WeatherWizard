49dff29abbd272c8177c4de345dbc0b8cc49533e
leo277 <yongkai8@hotmail.com>
2018-03-15 14:11:11 +0800

    Add styling

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Titles = props => (
 	<div>
-		<h1>Weather Finder</h1>
-		<p>Find out temperature and conditions</p>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<p className="title-container__subtitle">Find out temperature and conditions</p>
 	</div>
 );
 			
